8bb0fab11c25482e90e1b3470e226d37a23fe506
Luis Bonilla <blessedluismi@gmail.com>
2023-06-30 21:46:04 -0400

    Updating listing description

--- lib/pages/listing_description.dart
+++ lib/pages/listing_description.dart
@@ -31,8 +31,139 @@ class ListingDescription extends StatelessWidget {
                   '1-33 Park View Ave, Jersey City, NJ 07302',
                   style: GoogleFonts.poppins(
                       color: grayBasic, fontWeight: FontWeight.w400),
-                )
+                ),
               ],
+            ),
+            Container(
+              width: 340,
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                color: graybackground,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Rooms',
+                            style: GoogleFonts.poppins(
+                                color: grayBasic,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12),
+                          ),
+                          Text(
+                            '1b',
+                            style: GoogleFonts.poppins(
+                                color: flatblack,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Floor',
+                          style: GoogleFonts.poppins(
+                              color: grayBasic,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 12),
+                        ),
+                        Text(
+                          '24/40',
+                          style: GoogleFonts.poppins(
+                              color: flatblack,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Square',
+                          style: GoogleFonts.poppins(
+                              color: grayBasic,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 12),
+                        ),
+                        Text(
+                          '723 ft2',
+                          style: GoogleFonts.poppins(
+                              color: flatblack,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 20),
+              width: 340,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Monthly Rent',
+                        style: GoogleFonts.poppins(
+                            color: grayBasic,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12),
+                      ),
+                      Text(
+                        '\$3,600',
+                        style: GoogleFonts.poppins(
+                            color: flatblack,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 3.0),
+                        child: Text(
+                          '4.8',
+                          style: GoogleFonts.poppins(
+                              color: flatblack,
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      const HeroIcon(
+                        HeroIcons.star,
+                        style: HeroIconStyle.solid,
+                        color: yellow,
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -5,3 +5,4 @@ const demiblack = Color(0xFF1C1302);
 const graybackground = Color(0xffF7F6F2);
 const flatblack = Color(0xff1F1E1E);
 const grayBasic = Color(0xFF767271);
+const yellow = Color(0xFFF2B705);
